605cf69e63e8dc0ddb57895ea0bc32e640b4b2ab
mahiromiku <lucas25th@gmail.com>
2024-07-19 01:48:57 -0300

    localstorage

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,13 +1,13 @@
-import {createContext, useState, useEffect} from 'react'
+import { createContext, useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import {auth, db} from '../services/firebaseConnection'
-import {createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut} from 'firebase/auth'
-import {doc, getDoc, setDoc} from 'firebase/firestore'
-import {toast} from 'react-toastify'
+import { auth, db } from '../services/firebaseConnection'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth'
+import { doc, getDoc, setDoc } from 'firebase/firestore'
+import { toast } from 'react-toastify'
 
 export const AuthContext = createContext({})
 
-function AuthProvider({children}){
+function AuthProvider({ children }) {
     const [user, setUser] = useState(null)
     const [loadingAuth, setLoadingAuth] = useState(false)
     const [loading, setLoading] = useState(true)
@@ -15,92 +15,93 @@ function AuthProvider({children}){
     const navigate = useNavigate()
 
     useEffect(() => {
-        async function loadUser(){
+        async function loadUser() {
             const storageUser = localStorage.getItem('@ticketPRO')
-            if(storageUser){
+            if (storageUser) {
                 setUser(JSON.parse(storageUser))
-                setLoading(false)
             }
             setLoading(false)
         }
 
         loadUser()
     }, [])
-    
-    async function signIn(email, password){
+
+    async function signIn(email, password) {
         setLoadingAuth(true)
 
-        await signInWithEmailAndPassword(auth, email, password)
-        .then( async (value) => {
+        try {
+            const value = await signInWithEmailAndPassword(auth, email, password)
             let uid = value.user.uid
 
             const docRef = doc(db, 'users', uid)
             const docSnap = await getDoc(docRef)
 
-            let data = {
-                uid,
-                nome: docSnap.data().nome,
-                email: value.user.email,
-                avatarUrl: docSnap.data().avatarUrl
-            }
+            if (docSnap.exists()) {
+                let data = {
+                    uid,
+                    nome: docSnap.data().nome || '',
+                    email: value.user.email,
+                    avatarUrl: docSnap.data().avatarUrl || null
+                }
 
-            setUser(data)
-            storageUser(data)
-            setLoadingAuth(false)
-            toast.success('Bem-vindo de volta!')
-            navigate('/dashboard')
-        })
-        .catch((error) => {
+                setUser(data)
+                storageUser(data)
+                setLoadingAuth(false)
+                toast.success('Bem-vindo de volta!')
+                navigate('/dashboard')
+            } else {
+                throw new Error('Usuário não encontrado no banco de dados!')
+            }
+        } catch (error) {
             console.log(error)
             setLoadingAuth(false)
             toast.error('Ops! Algo deu errado!')
-        })
+        }
     }
 
-    async function signUp(email, password, name){
+    async function signUp(email, password, name) {
         setLoadingAuth(true)
 
-        await createUserWithEmailAndPassword(auth, email, password)
-        .then( async (value) => {
+        try {
+            const value = await createUserWithEmailAndPassword(auth, email, password)
             let uid = value.user.uid
 
             await setDoc(doc(db, 'users', uid), {
                 nome: name,
                 avatarUrl: null
             })
-            .then( () => {
-                let data = {
-                    uid,
-                    nome: name,
-                    email: value.user.email,
-                    avatarUrl: null
-                }
 
-                setUser(data)
-                storageUser(data)
-                setLoadingAuth(false)
-                toast.success('Seja bem-vindo ao sistema!')
-                navigate('/dashboard')
-            })
-        })
-        .catch((error) => {
+            let data = {
+                uid,
+                nome: name,
+                email: value.user.email,
+                avatarUrl: null
+            }
+
+            setUser(data)
+            storageUser(data)
+            setLoadingAuth(false)
+            toast.success('Seja bem-vindo ao sistema!')
+            navigate('/dashboard')
+        } catch (error) {
             console.log(error)
             setLoadingAuth(false)
-        })
+            toast.error('Ops! Algo deu errado!')
+        }
     }
 
-    function storageUser(data){
-        localStorage.setItem('@ticketsPRO', JSON.stringify(data))
+    function storageUser(data) {
+        localStorage.setItem('@ticketPRO', JSON.stringify(data))
     }
 
-    async function logOut(){
+    async function logOut() {
         await signOut(auth)
         localStorage.removeItem('@ticketPRO')
         setUser(null)
     }
 
-    return(
-        <AuthContext.Provider 
+    return (
+        <AuthContext.Provider
             value={{
                 signed: !!user,
                 user,
